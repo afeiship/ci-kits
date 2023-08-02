a7abacb859d147d336fef3a3e2325bb4a6e20e55
afeiship <1290657123@qq.com>
2023-08-02 08:45:58 +0800

    feat: add envname testcase

---
+++ __tests__/01-envname.spec.ts
@@ -0,0 +1,20 @@
+import { envname } from '../src';
+
+describe('01-api.envname', () => {
+  beforeEach(() => {
+    // unset CI_ENVIRONMENT_NAME
+    // unset npm_lifecycle_event
+    delete process.env.CI_ENVIRONMENT_NAME;
+    delete process.env.npm_lifecycle_event;
+  })
+
+  test('01.envname -> simulate CI_ENVIRONMENT_NAME', () => {
+    process.env.CI_ENVIRONMENT_NAME = 'test';
+    expect(envname()).toBe('test');
+  });
+
+  test('02.envname -> simulate npm_lifecycle_event', () => {
+    process.env.npm_lifecycle_event = 'build:dev';
+    expect(envname()).toBe('dev');
+  })
+});
